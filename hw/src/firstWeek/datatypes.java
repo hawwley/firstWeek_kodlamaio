cd330fb37c644c800d98b9c428e76ecc461d9937
hasan <hasan@DESKTOP-1IFBENN>
2022-09-20 22:17:18 +0300

    First Week

---
+++ hw/src/firstWeek/datatypes.java
@@ -0,0 +1,19 @@
+package firstWeek;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		//default olarak tam sayilar int, ondalikli sayilar double'dir.
+		
+		double sayi = 0.1;
+		sayi = -128;
+		
+		String sehir = "Ankara";
+		char karakter = 'A'; //char tek karakter tutar.
+		
+		boolean dogruMu=false;
+
+	
+	}
+
+}
--- hw/src/firstWeek/hellojava.java
+++ hw/src/firstWeek/hellojava.java
@@ -1,6 +1,6 @@
 package firstWeek;
 
-public class helloJava {
+public class hellojava {
 
 	public static void main(String[] args) {
 		
