2f9f26994c2323c57b2ab210d36a834f839917a8
hasan <hasan@DESKTOP-1IFBENN>
2022-09-20 23:00:17 +0300

    First Week

---
+++ hw/src/firstWeek/Switchdemo.java
@@ -0,0 +1,32 @@
+package firstWeek;
+
+public class Switchdemo {
+
+	public static void main(String[] args) {
+		char grade = 'F';
+		
+		switch(grade) {
+		case 'A' :
+			System.out.println("Mukemmel : Gectiniz");
+			break;
+		case 'B' :										//B ve C ayni olsun istersek ard arda yazariz.
+			System.out.println("Cok guzel : Gectiniz");
+			break;
+		case 'C' :
+			System.out.println("Iyi : Gectiniz");
+			break;
+		case 'D' :
+			System.out.println("Fena degil : Gectiniz");
+			break;
+		case 'F' :
+			System.out.println("Maalesef : Kaldiniz");
+			break;
+			default:
+				System.out.println("Gecersiz not girdiniz");
+				
+		
+		}
+
+	}
+
+}
