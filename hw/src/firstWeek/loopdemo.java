30fac01f0f23b25c612ec276eae91eb3374d176b
hasan <hasan@DESKTOP-1IFBENN>
2022-09-20 23:12:59 +0300

    First Week

--- hw/src/firstWeek/loopdemo.java
+++ hw/src/firstWeek/loopdemo.java
@@ -3,6 +3,24 @@ package firstWeek;
 public class loopdemo {
 
 	public static void main(String[] args) {
+		//For
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+			
+		}
+		System.out.println("Birer arttirma dongusu bitti.");
+		
+		for(int i=1;i<=10;i+=2) {
+			System.out.println(i);
+			
+		}
+		System.out.println("Ikiser arttirma dongusu bitti.");
+		
+		for(int i=0;i<10;i+=2) {
+			System.out.println(i);
+			
+		}
+		System.out.println("0-10 arasi cift sayi dongusu bitti.");
 		
 
 	}
