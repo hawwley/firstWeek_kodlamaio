aadb02ae429204a7f1d207c1dc8acfc5457ae53d
hasan <hasan@DESKTOP-1IFBENN>
2022-09-25 19:53:40 +0300

    First Week

---
+++ hw/src/firstWeek/DoWhile.java
@@ -0,0 +1,31 @@
+package firstWeek;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;  //i+=2 veya i=+2
+		}while (i<10); 
+		System.out.println("Do-While Dongusu Bitti.");
+		
+		
+		int j=100;  //100'u yazdirir,sonra sarti kontrol eder.
+		do {
+			System.out.println("Loglandi.");
+			System.out.println(j);
+			j++;  //i+=2 veya i=+2
+		}while (j<10); 
+		System.out.println("Do-While Dongusu Bitti.");
+		
+		//Dongu calismasa bile dongunun calistigina dair bir deneme yapmak icin kullanilabilir.
+			
+		
+		
+		
+		
+		
+	}
+
+}
